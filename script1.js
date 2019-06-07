468dc8900e7bf2afbb43aee7286540c8cb1a944f
alfonsoperezmanilla <48367249+alfonsoperezmanilla@users.noreply.github.com>
2019-06-07 00:17:39 -0500

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,561 @@
+function comenzar(){
+	var elemento = document.getElementById("lienzo1");
+	lienzo = elemento.getContext("2d");
+	//indica cuando inicia con un trazo
+	lienzo.beginPath();
+	//mueve el trazo a una cordenada
+	lienzo.moveTo(100,50);
+	//crea linea
+	lienzo.lineTo(100,50);
+	lienzo.lineTo(50,150);
+
+	//final de la linea
+	lienzo.lineTo(150,150);
+	//finalizar el trazo o cerrar dibujo
+	lienzo.closePath();
+
+	//crear mascara
+	lienzo.clip();
+
+	//lienzo.beginPath();
+	//for(i=0;i<250;i+=10){
+	//	lienzo.moveTo(0,i);
+	//	lienzo.lineTo(500,i);
+	//}
+
+	//lienzo.stroke();
+	lienzo.fill();
+
+
+}
+window.addEventListener("load",comenzar,false);
+
+//----------------------------------------------------------------------------------------------------------------------------
+
+function comenzar1(){
+	var elemento = document.getElementById("lienzo2");
+	lienzo1 = elemento.getContext("2d");
+	
+	//indica cuando inicia con un trazo
+	lienzo1.beginPath();
+	//mueve el trazo a una cordenada
+	lienzo1.moveTo(50,50);
+	//crea linea
+	lienzo1.lineTo(200,50);
+	lienzo1.lineTo(200,150);
+
+	//final de la linea
+	
+	lienzo1.lineTo(50,150);
+	//finalizar el trazo o cerrar dibujo
+	lienzo1.closePath();
+
+	//crear mascara
+	lienzo1.clip();
+
+	//lienzo.beginPath();
+	//for(i=0;i<250;i+=10){
+	//	lienzo.moveTo(0,i);
+	//	lienzo.lineTo(500,i);
+	//}
+
+	//lienzo1.stroke();
+	lienzo1.fill();
+}
+
+window.addEventListener("load",comenzar1,false);
+
+//----------------------------------------------------------------------------------------------------------------------------
+
+function comenzar3(){
+	var elemento = document.getElementById("lienzo4");
+	lienzo1 = elemento.getContext("2d");
+	
+	//indica cuando inicia con un trazo
+	lienzo1.beginPath();
+	//mueve el trazo a una cordenada
+	lienzo1.moveTo(50,50);
+	//crea linea
+	lienzo1.lineTo(200,50);
+	lienzo1.lineTo(160,150);
+
+	//final de la linea
+	
+	lienzo1.lineTo(40,150);
+	lienzo1.lineTo(85,50);
+	//finalizar el trazo o cerrar dibujo
+	lienzo1.closePath();
+
+	//crear mascara
+	lienzo1.clip();
+
+	//lienzo.beginPath();
+	//for(i=0;i<250;i+=10){
+	//	lienzo.moveTo(0,i);
+	//	lienzo.lineTo(500,i);
+	//}
+
+	//lienzo1.stroke();
+	lienzo1.fill();
+}
+
+window.addEventListener("load",comenzar3,false);
+
+//------------------------------------------------------------------------------------------------------------------------------
+function comenzar4(){
+	var elemento = document.getElementById("lienzo5");
+	lienzo1 = elemento.getContext("2d");
+	
+	//indica cuando inicia con un trazo
+	lienzo1.beginPath();
+	//mueve el trazo a una cordenada
+	lienzo1.moveTo(25,50);
+	//crea linea
+	lienzo1.lineTo(45,50);
+	lienzo1.lineTo(10,150);
+
+	//final de la linea
+	
+	lienzo1.lineTo(170,150);
+	lienzo1.lineTo(145,50);
+	//finalizar el trazo o cerrar dibujo
+	lienzo1.closePath();
+
+	//crear mascara
+	lienzo1.clip();
+
+	//lienzo.beginPath();
+	//for(i=0;i<250;i+=10){
+	//	lienzo.moveTo(0,i);
+	//	lienzo.lineTo(500,i);
+	//}
+
+	//lienzo1.stroke();
+	lienzo1.fill();
+}
+
+window.addEventListener("load",comenzar4,false);
+
+//------------------------------------------------------------------------------------------------------------------------------
+function comenzar5(){
+	var elemento = document.getElementById("lienzo6");
+	lienzo1 = elemento.getContext("2d");
+	
+	//indica cuando inicia con un trazo
+	lienzo1.beginPath();
+	//mueve el trazo a una cordenada
+	lienzo1.moveTo(100,50);
+	//crea linea
+	lienzo1.lineTo(100,50);
+	lienzo1.lineTo(50,95);
+
+	//final de la linea
+	
+	lienzo1.lineTo(100,150);
+	lienzo1.lineTo(145,95);
+	//finalizar el trazo o cerrar dibujo
+	lienzo1.closePath();
+
+	//crear mascara
+	lienzo1.clip();
+
+	//lienzo.beginPath();
+	//for(i=0;i<250;i+=10){
+	//	lienzo.moveTo(0,i);
+	//	lienzo.lineTo(500,i);
+	//}
+
+	//lienzo1.stroke();
+	lienzo1.fill();
+}
+window.addEventListener("load",comenzar5,false);
+
+//------------------------------------------------------------------------------------------------------------------------------
+
+function comenzar2(){
+	var elemento = document.getElementById("lienzo7");
+	lienzo3 = elemento.getContext("2d");
+	
+	//indica cuando inicia con un trazo
+	lienzo3.beginPath();
+	//mueve el trazo a una cordenada
+	lienzo3.moveTo(50,50);
+	//crea linea
+	lienzo3.lineTo(200,50);
+	lienzo3.lineTo(200,150);
+
+	//final de la linea
+	
+	lienzo3.lineTo(50,150);
+	//finalizar el trazo o cerrar dibujo
+	lienzo3.closePath();
+
+	//crear mascara
+	lienzo3.clip();
+
+	//lienzo3.beginPath();
+	//for(i=0;i<250;i+=10){
+	
+	//	lienzo3.moveTo(0,i);
+	//	lienzo3.lineTo(500,i);
+	//}
+
+	lienzo3.stroke();
+	//lienzo3.fill();
+}
+
+window.addEventListener("load",comenzar2,false);
+
+//-------------------------------------------------------------------------------------------------------------------------------
+
+function comenzar1(){
+	var elemento = document.getElementById("lienzo2");
+	lienzo1 = elemento.getContext("2d");
+	
+	//indica cuando inicia con un trazo
+	lienzo1.beginPath();
+	//mueve el trazo a una cordenada
+	lienzo1.moveTo(50,50);
+	//crea linea
+	lienzo1.lineTo(200,50);
+	lienzo1.lineTo(200,150);
+
+	//final de la linea
+	
+	lienzo1.lineTo(50,150);
+	//finalizar el trazo o cerrar dibujo
+	lienzo1.closePath();
+
+	//crear mascara
+	lienzo1.clip();
+
+	//lienzo.beginPath();
+	//for(i=0;i<250;i+=10){
+	//	lienzo.moveTo(0,i);
+	//	lienzo.lineTo(500,i);
+	//}
+
+	//lienzo1.stroke();
+	lienzo1.fill();
+}
+
+window.addEventListener("load",comenzar1,false);
+
+//------------------------------------------------------------------------------------------------------------------------------
+
+function comenzar9(){
+	var elemento = document.getElementById("lienzo8");
+	lienzo1 = elemento.getContext("2d");
+	
+	//indica cuando inicia con un trazo
+	lienzo1.beginPath();
+	//mueve el trazo a una cordenada
+	lienzo1.moveTo(50,50);
+	//crea linea
+	lienzo1.lineTo(200,50);
+	lienzo1.lineTo(160,150);
+
+	//final de la linea
+	
+	lienzo1.lineTo(40,150);
+	lienzo1.lineTo(85,50);
+	//finalizar el trazo o cerrar dibujo
+	lienzo1.closePath();
+
+	//crear mascara
+	lienzo1.clip();
+
+	//lienzo.beginPath();
+	//for(i=0;i<250;i+=10){
+	//	lienzo.moveTo(0,i);
+	//	lienzo.lineTo(500,i);
+	//}
+
+	lienzo1.stroke();
+	//lienzo1.fill();
+}
+
+window.addEventListener("load",comenzar9,false);
+
+//-------------------------------------------------------------------------------------------------------------------------------
+
+function comenzar11(){
+	var elemento = document.getElementById("lienzo9");
+	lienzo1 = elemento.getContext("2d");
+	
+	//indica cuando inicia con un trazo
+	lienzo1.beginPath();
+	//mueve el trazo a una cordenada
+	lienzo1.moveTo(25,50);
+	//crea linea
+	lienzo1.lineTo(45,50);
+	lienzo1.lineTo(10,150);
+
+	//final de la linea
+	
+	lienzo1.lineTo(170,150);
+	lienzo1.lineTo(145,50);
+	//finalizar el trazo o cerrar dibujo
+	lienzo1.closePath();
+
+	//crear mascara
+	lienzo1.clip();
+
+	//lienzo.beginPath();
+	//for(i=0;i<250;i+=10){
+	//	lienzo.moveTo(0,i);
+	//	lienzo.lineTo(500,i);
+	//}
+
+	lienzo1.stroke();
+	//lienzo1.fill();
+}
+
+window.addEventListener("load",comenzar11,false);
+
+//-----------------------------------------------------------------------------------------------------------------------------------
+
+function comenzar12(){
+	var elemento = document.getElementById("lienzo10");
+	lienzo1 = elemento.getContext("2d");
+	
+	//indica cuando inicia con un trazo
+	lienzo1.beginPath();
+	//mueve el trazo a una cordenada
+	lienzo1.moveTo(100,50);
+	//crea linea
+	lienzo1.lineTo(100,50);
+	lienzo1.lineTo(50,95);
+
+	//final de la linea
+	
+	lienzo1.lineTo(100,150);
+	lienzo1.lineTo(145,95);
+	//finalizar el trazo o cerrar dibujo
+	lienzo1.closePath();
+
+	//crear mascara
+	lienzo1.clip();
+
+	//lienzo.beginPath();
+	//for(i=0;i<250;i+=10){
+	//	lienzo.moveTo(0,i);
+	//	lienzo.lineTo(500,i);
+	//}
+
+	lienzo1.stroke();
+	//lienzo1.fill();
+}
+window.addEventListener("load",comenzar12,false);
+
+//------------------------------------------------------------------------------------------------------------------------------
+
+function comenzar15(){
+	var elemento = document.getElementById("lienzo3");
+	lienzo = elemento.getContext("2d");
+	//indica cuando inicia con un trazo
+	lienzo.beginPath();
+	//mueve el trazo a una cordenada
+	lienzo.moveTo(100,50);
+	//crea linea
+	lienzo.lineTo(100,50);
+	lienzo.lineTo(50,150);
+
+	//final de la linea
+	lienzo.lineTo(150,150);
+	//finalizar el trazo o cerrar dibujo
+	lienzo.closePath();
+
+	//crear mascara
+	lienzo.clip();
+
+	//lienzo.beginPath();
+	//for(i=0;i<250;i+=10){
+	//	lienzo.moveTo(0,i);
+	//	lienzo.lineTo(500,i);
+	//}
+
+	lienzo.stroke();
+	//lienzo.fill();
+
+
+}
+window.addEventListener("load",comenzar15,false);
+
+//------------------------------------------------------------------------------------------------------------------------------------
+
+function comenzar16(){
+	var elemento = document.getElementById("lienzo11");
+	lienzo = elemento.getContext("2d");
+	//indica cuando inicia con un trazo
+	lienzo.beginPath();
+	//mueve el trazo a una cordenada
+	lienzo.moveTo(100,50);
+	//crea linea
+	lienzo.lineTo(100,50);
+	lienzo.lineTo(50,150);
+
+	//final de la linea
+	lienzo.lineTo(150,150);
+	//finalizar el trazo o cerrar dibujo
+	lienzo.closePath();
+
+	//crear mascara
+	lienzo.clip();
+
+	lienzo.beginPath();
+	for(i=0;i<250;i+=10){
+		lienzo.moveTo(0,i);
+		lienzo.lineTo(500,i);
+	}
+
+	lienzo.stroke();
+	//lienzo.fill();
+
+
+}
+window.addEventListener("load",comenzar16,false);
+
+//------------------------------------------------------------------------------------------------------------------------------
+
+
+function comenzar20(){
+	var elemento = document.getElementById("lienzo12");
+	lienzo1 = elemento.getContext("2d");
+	
+	//indica cuando inicia con un trazo
+	lienzo1.beginPath();
+	//mueve el trazo a una cordenada
+	lienzo1.moveTo(50,50);
+	//crea linea
+	lienzo1.lineTo(200,50);
+	lienzo1.lineTo(200,150);
+
+	//final de la linea
+	
+	lienzo1.lineTo(50,150);
+	//finalizar el trazo o cerrar dibujo
+	lienzo1.closePath();
+
+	//crear mascara
+	lienzo1.clip();
+
+	lienzo1.beginPath();
+	for(i=0;i<250;i+=10){
+		lienzo1.moveTo(0,i);
+		lienzo1.lineTo(500,i);
+	}
+
+	lienzo1.stroke();
+	//lienzo1.fill();
+}
+
+window.addEventListener("load",comenzar20,false);
+
+//--------------------------------------------------------------------------------------------------------------------------------
+
+function comenzar23(){
+	var elemento = document.getElementById("lienzo13");
+	lienzo1 = elemento.getContext("2d");
+	
+	//indica cuando inicia con un trazo
+	lienzo1.beginPath();
+	//mueve el trazo a una cordenada
+	lienzo1.moveTo(50,50);
+	//crea linea
+	lienzo1.lineTo(200,50);
+	lienzo1.lineTo(160,150);
+
+	//final de la linea
+	
+	lienzo1.lineTo(40,150);
+	lienzo1.lineTo(85,50);
+	//finalizar el trazo o cerrar dibujo
+	lienzo1.closePath();
+
+	//crear mascara
+	lienzo1.clip();
+
+	lienzo1.beginPath();
+	for(i=0;i<250;i+=10){
+		lienzo1.moveTo(0,i);
+		lienzo1.lineTo(500,i);
+	}
+
+	lienzo1.stroke();
+	//lienzo1.fill();
+}
+
+window.addEventListener("load",comenzar23,false);
+
+//--------------------------------------------------------------------------------------------------------------------------------
+
+function comenzar24(){
+	var elemento = document.getElementById("lienzo14");
+	lienzo1 = elemento.getContext("2d");
+	
+	//indica cuando inicia con un trazo
+	lienzo1.beginPath();
+	//mueve el trazo a una cordenada
+	lienzo1.moveTo(25,50);
+	//crea linea
+	lienzo1.lineTo(45,50);
+	lienzo1.lineTo(10,150);
+
+	//final de la linea
+	
+	lienzo1.lineTo(170,150);
+	lienzo1.lineTo(145,50);
+	//finalizar el trazo o cerrar dibujo
+	lienzo1.closePath();
+
+	//crear mascara
+	lienzo1.clip();
+
+	lienzo1.beginPath();
+	for(i=0;i<250;i+=10){
+		lienzo1.moveTo(0,i);
+		lienzo1.lineTo(500,i);
+	}
+
+	lienzo1.stroke();
+	//lienzo1.fill();
+}
+
+window.addEventListener("load",comenzar24,false);
+
+//-------------------------------------------------------------------------------------------------------------------------------
+
+function comenzar25(){
+	var elemento = document.getElementById("lienzo15");
+	lienzo1 = elemento.getContext("2d");
+	
+	//indica cuando inicia con un trazo
+	lienzo1.beginPath();
+	//mueve el trazo a una cordenada
+	lienzo1.moveTo(100,50);
+	//crea linea
+	lienzo1.lineTo(100,50);
+	lienzo1.lineTo(50,95);
+
+	//final de la linea
+	
+	lienzo1.lineTo(100,150);
+	lienzo1.lineTo(145,95);
+	//finalizar el trazo o cerrar dibujo
+	lienzo1.closePath();
+
+	//crear mascara
+	lienzo1.clip();
+
+	lienzo1.beginPath();
+	for(i=0;i<250;i+=10){
+		lienzo1.moveTo(0,i);
+		lienzo1.lineTo(500,i);
+	}
+
+	lienzo1.stroke();
+	//lienzo1.fill();
+}
+window.addEventListener("load",comenzar25,false);
+
